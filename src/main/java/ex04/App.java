cffceaf2512e3ebf718cc7f24be5a618f80eeec9
Naomi <naomi.m.palm@gmail.com>
2021-06-06 01:38:04 -0400

    Initial commit

---
+++ src/main/java/ex04/App.java
@@ -0,0 +1,32 @@
+package ex04;
+
+import java.util.Scanner;
+
+public class App {
+
+    static Scanner input = new Scanner(System.in);
+
+    public static void main (String[] args){
+
+        App newApp = new App();
+
+        System.out.println("Enter a noun: ");
+        String noun = input.nextLine();
+
+        System.out.println("Enter a verb: ");
+        String verb = input.nextLine();
+
+        System.out.println("Enter a adjective: ");
+        String adjective = input.nextLine();
+
+        System.out.println("Enter a adverb: ");
+        String adverb = input.nextLine();
+
+        String outPut = newApp.stringConCat(noun, verb, adjective, adverb);
+
+        System.out.println(outPut);
+    }
+    private String stringConCat(String noun, String verb, String adjective, String adverb){
+        return "Do you " + verb + " your " + adjective + " " + noun + " " + adverb + " ? That's hilarious!";
+    }
+}
